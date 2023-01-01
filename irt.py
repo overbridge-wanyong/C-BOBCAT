d2cc269560c99994ee3a89e4052fdb6f52a87ecc
WanyongFeng <wanyongfeng123@gmail.com>
2023-01-01 14:48:01 -0500

    Update irt.py

--- irt.py
+++ irt.py
@@ -15,13 +15,9 @@ from dataset import Dataset, collate_fn
 from utils.utils import open_json, dump_json, compute_auc, compute_accuracy, data_split, batch_accuracy, list_to_string
 from utils.configuration import create_parser, initialize_seeds
 import time
-#import wandb
 import neptune
 import os
-#running in cluster
 DEBUG = False  # if torch.cuda.is_available() else True
-# if DEBUG:
-#os.environ["WANDB_MODE"] = "dryrun"
 
 
 def sigmoid(x):
@@ -71,28 +67,54 @@ def convert_to_irt(data):
     return Xs, Ys
 
 
-def test_model(id_, split='val'):
+def test_model(id_, split='val', get_rate = False):
     if split == 'val':
         valid_dataset.seed = id_
     elif split == 'test':
         test_dataset.seed = id_
     loader = torch.utils.data.DataLoader(
         valid_dataset if split == 'val' else test_dataset, collate_fn=collate_fn, batch_size=params.test_batch_size, num_workers=num_workers, shuffle=False, drop_last=False)
-
+    selected, occurrence = torch.zeros(1, params.n_question), 1e-12+torch.zeros(1, params.n_question)
+    random_probs = torch.zeros(1, params.n_question)
+    exposure_rates = None
     total_loss, all_preds, all_targets = 0., [], []
     n_batch = 0
     for batch in loader:
-        output = model.forward(batch)
+        output, picked_samples, available_samples = model.forward(batch)
         target = batch['output_labels'].float().numpy()
         mask = batch['output_mask'].numpy() == 1
         all_preds.append(output[mask])
         all_targets.append(target[mask])
+
+        if get_rate:
+            selected.scatter_add_(1, picked_samples[:,1][None,:], torch.ones(1,picked_samples.shape[0]))
+            occurrence = occurrence + torch.sum(available_samples, dim =0, keepdim=True)
+            temp = (params.n_query*available_samples) / torch.sum(available_samples, dim=-1, keepdim=True)
+            random_probs = random_probs +  torch.sum(temp, dim=0)
+
         n_batch += 1
+        
     all_pred = np.concatenate(all_preds, axis=0)
     all_target = np.concatenate(all_targets, axis=0)
     auc = compute_auc(all_target, all_pred)
     accuracy = compute_accuracy(all_target, all_pred)
-    return total_loss/n_batch, auc, accuracy
+    
+    # rates
+    if get_rate:
+        selected, occurrence  = selected.squeeze(0), occurrence.squeeze(0)
+        exposure_rate = selected/occurrence
+        mu, std = torch.mean(exposure_rate), torch.std(exposure_rate)
+        histogram  = torch.histogram(exposure_rate.cpu(),bins=101, range=(-0.01+1e-6,1.+1e-6))[0].numpy().tolist()
+        random_probs /= occurrence
+        chi_square = torch.mean((exposure_rate -random_probs)**2. /(random_probs +1e-20))
+        exposure_rates = {'mu':mu, 'std':std, 'histogram':histogram, 'chi':float(chi_square)}
+        #overlap
+        num =  torch.sum(selected * torch.clamp(selected-1., min=0))
+        p =  torch.sum(selected)/params.n_query
+        denom = params.n_query * p *(p-1.)
+        exposure_rates['overlap_mu']= float(num/denom)
+    
+    return total_loss/n_batch, auc, accuracy, exposure_rates
 
 
 class Model:
@@ -107,18 +129,24 @@ class Model:
         student_thetas = np.array([avg_theta] * nb_students).reshape(-1, 1)
         results = defaultdict(list)
         if self.sampling == 'active':
-            input_mask = batch['input_mask'].numpy()
+            input_mask = batch['input_mask'].detach().clone().numpy()
+            available_samples = batch['input_mask'].detach().clone()
+            picked_samples = torch.zeros(nb_students, self.n_question)
             for _ in range(self.n_query):
                 pr = proba(student_thetas, difficulty)
                 loss = np.abs(0.5 - pr) + (1 - input_mask)
                 selections = np.argmin(loss, axis=1)
                 input_mask[range(nb_students), selections] = 0
-                #
+                picked_samples[range(nb_students), selections] = 1
                 for user_id, item_id in enumerate(selections):
                     results[user_id].append(
                         (difficulty[item_id], input_labels[user_id, item_id]))
                 for user_id in range(nb_students):
                     student_thetas[user_id] = estimated_theta(results[user_id])
+                    
+            picked_samples = picked_samples.nonzero()#student_idx, q_idx
+            
+            
         elif self.sampling == 'random':
             input_mask = batch['input_mask']
             if self.n_query == -1:
@@ -129,6 +157,9 @@ class Model:
                     input_mask.float(), self.n_query, replacement=False)
                 train_mask = train_mask.scatter(
                     dim=1, index=selections, value=1)
+                picked_samples = train_mask.detach().clone()
+                picked_samples = picked_samples.nonzero()
+                available_samples = batch['input_mask'].detach().clone()
                 train_mask = train_mask.numpy()
             user_ids, item_ids = np.where(train_mask > 0)
             for user_id, item_id in zip(user_ids, item_ids):
@@ -138,18 +169,18 @@ class Model:
                 student_thetas[user_id] = estimated_theta(results[user_id])
         # Test
         output = proba(student_thetas, difficulty)
-        return output
+        return output, picked_samples, available_samples
 
 
 if __name__ == "__main__":
     params = create_parser()
     print(params)
-    project = "arighosh/bobcat"
+    project = "" # need to add neptune project name
     initialize_seeds(params.seed)
     #
     data_path = os.path.normpath('data/train_task_'+params.dataset+'.json')
     train_data, valid_data, test_data = data_split(
-        data_path, params.fold, params.subset, params.seed)
+        data_path, params.fold, params.seed)
     valid_dataset, test_dataset = Dataset(valid_data), Dataset(test_data)
     n_users, n_question = len(train_data), params.n_question
     for idx, d in enumerate(train_data):
@@ -157,7 +188,7 @@ if __name__ == "__main__":
     Xs, Ys = convert_to_irt(train_data)
 
     # Train
-    lr = LogisticRegression(solver='lbfgs', C=1./params.lr,
+    lr = LogisticRegression(solver='lbfgs', C= 1e30,
                             max_iter=1000, fit_intercept=False)
     lr.fit(Xs, Ys)
     weights = lr.coef_[0]
@@ -185,31 +216,34 @@ if __name__ == "__main__":
     best_val_score, best_test_score = 0, 0
     best_val_auc, best_test_auc = 0, 0
 
-    # for policy_lr in []:
     for policy_lr in [1e1, 1,  1e-1, 1e-2, 1e-4, 0]:
         params.policy_lr = policy_lr
         # Validation
         val_scores, val_aucs = [], []
         test_scores, test_aucs = [], []
         for idx in N:
-            _, auc, acc = test_model(id_=idx, split='val')
+            _, auc, acc, _ = test_model(id_=idx, split='val', get_rate=False)
             val_scores.append(acc)
             val_aucs.append(auc)
         val_score = sum(val_scores)/(len(N)+1e-20)
         val_auc = sum(val_aucs)/(len(N)+1e-20)
-        print('validation', val_score, policy_lr)
         if best_val_score < val_score:
             best_policy_lr = policy_lr
             best_val_score = val_score
             best_val_auc = val_auc
             # Run on test set
             for idx in N:
-                _, auc, acc = test_model(id_=idx, split='test')
+                _, auc, acc, exposure_rates = test_model(id_=idx, split='test', get_rate=idx==N[-1])
                 test_scores.append(acc)
                 test_aucs.append(auc)
             best_test_score = sum(test_scores)/(len(N)+1e-20)
             best_test_auc = sum(test_aucs)/(len(N)+1e-20)
-            print('Testing', best_test_score, policy_lr)
+            print('Testing acc', best_test_score, policy_lr)
+            print('Testing auc', best_test_auc, policy_lr)
+            print("expousre phi", exposure_rates['chi'])
+            print("overlap mu", exposure_rates['overlap_mu'])
+            
+            
 
     print('Best Test score: {}, Best Valid Score:{}, best policy lr: {}'.format(
         best_test_score, best_val_score, best_policy_lr))
